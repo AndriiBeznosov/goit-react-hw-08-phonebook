2ac09be4b65109b7f6d351a9a744df633a8a573a
Andrii Beznosov <andrey301288@gmail.com>
2022-11-20 11:49:14 +0200

    add toolkit,redux-persist

--- src/utils/LocalStorage/GetItem.jsx
+++
@@ -1,4 +0,0 @@
-export const GetItemLocalStorage = item => {
-  const contactsList = JSON.parse(localStorage.getItem(item));
-  return contactsList;
-};
--- src/utils/LocalStorage/SetItem.jsx
+++
@@ -1,4 +0,0 @@
-export const SetItemLocalStorage = (key, item) => {
-  localStorage.setItem(key, JSON.stringify(item));
-  return;
-};
